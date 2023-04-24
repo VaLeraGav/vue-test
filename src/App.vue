1f08979e0bc3fb36dfb6801acea242948e50f2c2
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2023-04-24 18:48:58 +0300

    fix: add search and filter

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,35 @@
   <div class="app">
     <h2>Страница с постами</h2>
 
-    <my-button
-        @click="showDialog"
-        style="margin: 15px 0;"
-    >
-      Создать пост
-    </my-button>
+    <my-input
+        v-model="searchQuery"
+        placeholder="Поиск..."
+    />
+
+    <div class="add__btns">
+      <my-button
+          @click="showDialog"
+      >
+        Создать пост
+      </my-button>
+      <my-select
+          v-model="selectedSort"
+          :options="sortOption"
+      />
+    </div>
 
     <my-dialog v-model:show="dialogVisible">
-      <!--    @create - подписались на событие create -->
-      <!--    this.$emit('create', this.post) в PostForm -->
       <post-form
           @create="createPost"
       />
     </my-dialog>
 
     <post-list
-        :posts="posts"
+        :posts="sortedAndSearchedPosts"
         @remove="removePost"
+        v-if="!isPostsLoading"
     />
+    <div v-else>Идет загрузка...</div>
 
   </div>
 </template>
@@ -31,21 +41,29 @@ import PostList from "@/components/PostList";
 import PostForm from "@/components/PostForm";
 import MyDialog from "@/components/ui/MyDialog";
 import MyButton from "@/components/ui/MyButton";
+import axios from "axios";
+import MySelect from "@/components/ui/MySelect";
+import MyInput from "@/components/ui/MyInput";
 
 export default {
   components: {
+    MyInput,
+    MySelect,
     MyButton,
     MyDialog,
     PostForm, PostList
   },
   data() {
     return {
-      posts: [
-        {id: 1, title: 'Vue.js', body: 'Описание поста',},
-        {id: 2, title: 'PHP', body: 'Описание поста',},
-        {id: 3, title: 'JS', body: 'Описание поста',}
-      ],
-      dialogVisible: false
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: '',
+      searchQuery: '',
+      sortOption: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По названию'}
+      ]
     }
   },
   methods: {
@@ -58,13 +76,58 @@ export default {
     removePost(post) {
       this.posts = this.posts.filter(p => p.id !== post.id)
     },
+
     showDialog() {
       this.dialogVisible = true;
+    },
+
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        this.posts = response.data;
+
+      } catch (e) {
+        alert('Ошибка')
+      } finally {
+        this.isPostsLoading = false;
+      }
     }
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+
+  // всегда возвращает какое-то вычисление, возвращаться единожды
+  computed: {
+    sortedPosts() {
+      return [...this.posts.sort((post1, post2) =>
+          post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+      ];
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery));
+    }
+  },
+
+  // отрабатывает когда произошло изменение
+  watch: {
+    // selectedSort(newValue) {
+    //   this.posts.sort((post1, post2) => {
+    //     return post1[newValue]?.localeCompare(post2[newValue])
+    //   })
+    // }
   }
 }
 </script>
 
 <style>
 @import '/public/style.css';
+
+.add__btns {
+  display: flex;
+  justify-content: space-between;
+  margin: 15px 0;
+}
 </style>
--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -42,6 +42,15 @@ export default {
     }
   },
 
+  // watch: {
+  //   post: {
+  //     handler(newValue) {
+  //       console.log(newValue);
+  //     },
+  //     deep: true
+  //   }
+  // }
+
 }
 </script>
 
